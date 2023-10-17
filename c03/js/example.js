16fbd7ba8cb221d298eba23901076f23bb770214
mkppham <mokiphamtb@gmail.com>
2023-10-17 23:35:10 +0700

    .

--- c03/js/example.js
+++ c03/js/example.js
@@ -27,6 +27,17 @@
         var weekFromToday, day, date, month, year, dayNames, monthNames;
         weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
         dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        monthNames = []
+        monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        day = dayNames[weekFromToday.getDay()];
+        date = weekFromToday.getDate();
+        month = monthNames[weekFromToday.getMonth()];
+        year = weekFromToday.getFullYear();
+        expiryMsg = 'offer expires next ';
+        expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+        return expiryMsg;
     }
-})
+
+    today = new Date();
+    elEnds = document.getElementById=('offerEnds');
+    elEnds.innerHTML = offerExpires(today);
+}());
