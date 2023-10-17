3b3ec950e23efd422072ecaad69556a585c71f64
mkppham <mokiphamtb@gmail.com>
2023-10-17 13:31:20 +0700

    .

---
+++ c03/js/example.js
@@ -0,0 +1,32 @@
+(function() {
+    var hotel = {
+        name: 'Park',
+        roomRate: 240,
+        discount: 15,
+        offerPrice: function(){
+            var offerRate = this.roomRate * ((100 - this.discount) / 100);
+            return offerRate;
+        }
+    };
+
+    var hotelName, roomRate, specialRate;
+    hotelName = document.getElementById('hotelName');
+    roomRate = document.getElementById('roomRate');
+    specialRate = document.getElementById("specialRate");
+
+    hotelName.textContent = hotel.name;
+    roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+    specialRate.textContent  = '$' + hotel.offerPrice();
+
+    var expiryMsg;
+    var today;
+    var elEnds;
+
+    function offerExpires(today) {
+
+        var weekFromToday, day, date, month, year, dayNames, monthNames;
+        weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+        dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        monthNames = []
+    }
+})
