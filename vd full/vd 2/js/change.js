67ec7ca8b18a8d2bf50e91d2d92c14280e7f7198
mkppham <mokiphamtb@gmail.com>
2023-10-19 08:31:21 +0700

    .

--- vd full/vd 2/js/change.js
+++ vd full/vd 2/js/change.js
@@ -2,64 +2,65 @@ function updateGreeting() {
     // var test = 1;
     // var test;
     // test = 2;
-    // var: phạm vi toàn cục(global),
-    // có thể khai báo lại, không cần gán giá trị khi khởi tạo 
-    // có thể thay đổi cập nhật giá trị
+    // var: phạm vi toàn cục (golbal),
+    // có thể khai báo lại, không cần giá trị khi khởi tạo
+    // có thể tay đổi cập nhật giá trị
 
-    // const khai báo hằng số, giá trị không được cập nhật và PHẢI khởi tạo giá trị ban đầu
+    //const khai báo hằng số, giá trị không được cập nhật và PHẢI khởi tạo giá trị ban đầu
     const greetingElement = document.getElementById('greeting');
     // const greetingElement;
-    // const greetingELement1 = '';
-    const timeElement = document.getElementById('time');
-    const timeElement2 = document.getElementById('time2');
+    // const greetingElement1 = '';
+    const timeElenment = document.getElementById('time');
+    const timeElenment2 = document.getElementById('time2');
     const secondsElement = document.querySelector('.seconds span');
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
 
-    // let khai báo biến, không được khai báo lại, không cần gán giá trị ban đầu 
+    // let khai báo biến, không được khai báo lại, có thể cập nhật giá trị
     let greeting = '';
     let className = '';
-    // let greeting;
+    // let greeing;
     // let className;
-    
+
     if (currentHour < 12) {
-        greeting = 'Chào buổi sáng';
+        greeting = 'Chao buoi sang';
         className = 'morning';
     } else if (currentHour < 18) {
-        greeting = 'Chào buổi chiều';
+        greeting = 'Chao buoi chieu';
         className = 'afternoon';
     } else {
-        greeting = 'Chào buổi tối';
+        greeting = 'Chao buoi toi';
         className = 'evening';
     }
 
     greetingElement.textContent = greeting;
     greetingElement.className = className;
 
-    const timeString = `Thời gian đầy đủ:: ${curentTime}`;
-    const timeString2 = `Thời gian rút gọn: ${curentTime.getHours()}:${String(currentTime.getMinutes()).padStart(2, '0')}:${String(currentTime.getSeconds()).padStart(2, '0')}`;
-    timeElement.textContent = timeString;
-    timeElement2.textContent = timeString2;
+    const timeString = `Thoi gian day du: ${currentTime}`;
+    const timeString2 = `Thoi gian rut gon: ${currentTime.getHours()}:${String(currentTime.getMinutes()).padStart(2, '0')}:${String(currentTime.getSeconds()).padStart(2, '0')}`;
+    timeElenment.textContent = timeString;
+    timeElenment2.textContent = timeString2;
 
     secondsElement.textContent = currentTime.getSeconds();
 }
-// Gọi hàm cập nhật mỗi giây 
+
+// Gọi hàm cập nhật mỗi giây
 setInterval(updateGreeting, 1000);
 
-// Cập nhật lần đầu khi tải trang
+// cập nhật lần đầu khi tải trang
 updateGreeting();
 
 function changeColor() {
-    const timeElement = document.getElementById('text');
-    //Lấy ra màu sắc hiện tại của phần tử 
-    const curentColor = textElement.style.color;
-
-    if (curentColor === 'blue') {
+    const textElement = document.getElementById('text');
+    // lấy ra màu sắc hiện tại của phần tử
+    const currentColor = textElement.style.color;
+    
+    if (currentColor === 'blue') {
         textElement.style.color = 'red';
-        textElement.textContent = 'văn bản màu đỏ rồi sẽ thay màu ';
+        textElement.textContent = 'Văn bản màu đỏ rồi sẽ thay màu';
     } else {
         textElement.style.color = 'blue';
-        textElement.textContent = 'văn bản màu xanh rồi sẽ thay màu ';
+        textElement.textContent = 'Văn bản màu xanh rồi sẽ thay màu';
     }
 }
 
