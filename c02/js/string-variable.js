1031da724ba0545da5324bb33ca12c569298a02f
mkppham <mokiphamtb@gmail.com>
2023-10-23 11:32:00 +0700

    .

--- c02/js/string-variable.js
+++ c02/js/string-variable.js
@@ -1,12 +1,17 @@
-// 
+// Tạo biến để giữ tên và ghi chú văn bản
 var username;
 var message;
 
+// Gán giá trị cho các biến này.
 username = 'Molly';
 message = 'See our upcoming range';
 
+// Lấy phần tử có id name.
 var elName = document.getElementById('name');
+// Thay thế nội dung của phần tử này.
 elName.textContent = username;
 
+// Lấy phần tử có id note.
 var elNote = document.getElementById('note');
+// Thay thế nội dung phần tử này.
 elNote.textContent = message;
